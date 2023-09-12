7f2ef425f5c9f1c12263cd282c04bdebdbad1f70
Grazielle <grazielleconceicao@yahoo.com.br>
2023-09-11 22:06:58 -0300

    feat: Projeto iniciado

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener("load", function () {
+    const button = document.getElementById("button");
+
+    button.onclick = function () {
+        const inputDay = document.getElementById("day");
+        const day = inputDay.value;
+
+    }
+});
